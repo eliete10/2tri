75db7db5b1a1cc99d40a27bed7a26f3a2c256c6e
eliete10 <163903154+eliete10@users.noreply.github.com>
2024-08-16 15:23:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const caixaPrincipal = document.querySelector(".caixa-principal")
-const caixaPerguntas = document.querySelector("caixa-perguntas")
-const caixa
-const
-const
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas =[
+    {
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia,todas as dúvidas de que uma pode ter, ele também gera imagens e áudios hiper-realistas.qual o primeiro pensamento?",
+    alternativas: [ 
+         "alternativa 1",
+        "aternativa 2",
+     ]
+},
+{
+    enunciado: "Com a descoberta desta tecnologia, chamada inteligência artificial",
+    alternativas: [
+        "Alternativa 1",
+        "Atenativa 2",
+    ]
+},
+{
+    enunciado: "pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Atenativa 2",
+    ]
+}
+];
