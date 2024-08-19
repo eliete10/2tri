d269e9ded315a5a9637fd0c679a74665671bcf20
eliete10 <163903154+eliete10@users.noreply.github.com>
2024-08-19 15:21:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,18 +12,19 @@ const perguntas =[
         "aternativa 2",
      ]
 },
+{    enunciado: "Com a descoberta desta tecnologia, chamada inteligência artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aula sobra elaIA. Escreva uma trabalho em sala de aula. qual atitude você toma?"
+
+    alternativas: [
+        "Utilizar uma ferramenta de buscar na innternet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e exprique numa linguagem que facilite o entendimento",
+        "Escrever o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+    ]
+},
 {
-    enunciado: "Com a descoberta desta tecnologia, chamada inteligência artificial",
+    enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre ",
     alternativas: [
         "Alternativa 1",
         "Atenativa 2",
     ]
 },
-{
-    enunciado: "pergunta 1",
-    alternativas: [
-        "Alternativa 1",
-        "Atenativa 2",
-    ]
-}
+
 ];
